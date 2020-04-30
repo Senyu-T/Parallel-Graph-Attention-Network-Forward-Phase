4faca282ef5cddb7150f1dd099dcaff464fd6e5d
Yile Liu <yilel@Yiles-MacBook-Pro.local>
2020-04-30 16:38:50 -0400

    fixed option

--- run.c
+++ run.c
@@ -8,9 +8,7 @@
 int main(int argc, char *argv[]) {
     //char c;
     FILE *gfile, *lfile;
-    gfile = fopen("data/simple_5_3.txt", "r");
-    lfile = fopen("data/layer_2_3_4.txt", "r");
-    int thread_count = 1;
+    gfile = fopen("/afs/andrew.cmu.edu/usr7/yilel/private/15418/Parallel-Graph-Attention-Network-Forward-Phase/data/graph_2048_800000_2048.txt", "r");
     int check_correctness = 0;
 
     char *optstring = "f:l:t:c";
@@ -22,9 +20,6 @@ int main(int argc, char *argv[]) {
                 break;
             case 'l':
                 lfile = fopen(optarg, "r");
-            case 't':
-                thread_count = atoi(optarg);
-                break;
             case 'c':
                 check_correctness = 1;
                 break;
@@ -33,16 +28,19 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    omp_set_num_threads(thread_count);
     graph_t *g = read_graph(gfile);
 
     //read in weights, a, nhead
     if (check_correctness){
+        gfile = fopen("/afs/andrew.cmu.edu/usr7/yilel/private/15418/Parallel-Graph-Attention-Network-Forward-Phase/data/simple_5_3.txt", "r");
+        lfile = fopen("/afs/andrew.cmu.edu/usr7/yilel/private/15418/Parallel-Graph-Attention-Network-Forward-Phase/data/layer_2_3_4.txt", "r");
+
+        graph_t *g = read_graph(gfile);
         layer_t *new_layer = read_layer(lfile, g->nnode, g->nedge);
 //        layer_t *new_layer = layer_init(in, out, g->nnode, g->nedge, nheads);
         forward(new_layer, g);
 
-        int out = g->nfeature;
+        int out =  g->nfeature;
         FILE *out_file = fopen("/afs/andrew.cmu.edu/usr7/yilel/private/15418/Parallel-Graph-Attention-Network-Forward-Phase/data/c_output.txt", "w+");
         char tmp[50];
         for (int i=0; i<g->nnode; i++){
